6f4f4806e961ba4fd4baf6406c4710d0c0c2795c
NguyenCaoTri2003 <caotri2000mwg@gmail.com>
2026-03-10 15:38:50 +0700

    fix

--- FE_QLGIAONHAN/src/environments/environment.ts
+++ FE_QLGIAONHAN/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: "http://localhost:5000/api"
-  // API_URL: "https://qlgiaonhan.onrender.com/api"
+  // API_URL: "http://localhost:5000/api"
+  API_URL: "https://qlgiaonhan.onrender.com/api"
 };
