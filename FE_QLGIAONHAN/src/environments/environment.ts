8e5ec333fb8d97297a35cccbe264e1e96fd16024
NguyenCaoTri2003 <caotri2000mwg@gmail.com>
2026-03-12 08:21:35 +0700

    change enviriment

--- FE_QLGIAONHAN/src/environments/environment.ts
+++ FE_QLGIAONHAN/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: "http://localhost:5000/api",
-  BE_URL: "http://localhost:5000"
-  // API_URL: "https://qlgiaonhan.onrender.com/api"
+  // API_URL: "http://localhost:5000/api",
+  // BE_URL: "http://localhost:5000"
+  API_URL: "https://qlgiaonhan.onrender.com/api",
+  BE_URL: "https://qlgiaonhan.onrender.com"
 };
